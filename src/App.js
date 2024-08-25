99613a385ca36cd27fa907576f26bfbfb9374535
Aashish Sawant <aashish.sawant@nagarro.com>
2024-08-25 14:56:32 +0530

    Configure React app for deployment to GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [response, setResponse] = useState(null);  // Use null initially since it's a single object, not an array
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const result = await axios.get(`http://127.0.0.1:8000/`);
+        setResponse(result.data);  // Set the response data to state
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    getData();  // Call the function to fetch data
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Response from API:</h1>
+      <pre>{JSON.stringify(response, null, 2)}</pre>  {/* Display the response in a formatted JSON format */}
     </div>
   );
 }
